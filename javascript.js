cb2d50db10437e56d3a0d94ed11e3f71ce3b4a9a
Matheus <matheushagedorn@univille.br>
2024-02-15 14:50:24 -0300

    formulario

---
+++ javascript.js
@@ -0,0 +1,24 @@
+function bloqueiaCaractere(input) {
+  var n = /[^0-9]/ | ["-"];
+  n.lastIndex = 0;
+  if (n.test(input.value)) {
+    input.value = "";
+  }
+}
+
+function calculaRenda() {
+  var inputsRenda = document.querySelectorAll('[id="renda"]');
+  var total = 0;
+
+  inputsRenda.forEach(function (input) {
+    var valor = parseFloat(input.value);
+    if (!isNaN(valor)) {
+      total += valor;
+    }
+  });
+  
+  var valorDividido = total / 2;
+
+  document.getElementById('resultado').value = total;
+  document.getElementById('resultado2').value = valorDividido;
+}
